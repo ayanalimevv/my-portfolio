989f0a5e1a669685caf901cf29967d204bf59e11
ayan <ayan787ali.mev@gmail.com>
2024-10-09 09:20:54 +0530

    Refactor react-router imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // App.tsx
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './pages/HomePage';
 import Socials from './pages/SocialsPage';
